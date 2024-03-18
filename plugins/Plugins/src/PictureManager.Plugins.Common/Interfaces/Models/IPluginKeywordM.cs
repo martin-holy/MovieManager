17086aa61974f8e9c3d7b495a8692050c6c99217
martin-holy <martin.holy@hotmail.com>
2024-03-18 16:20:35 +0100

    [C] MovieManager update

--- plugins/Plugins/src/PictureManager.Plugins.Common/Interfaces/Models/IPluginKeywordM.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace PictureManager.Plugins.Common.Interfaces.Models;
-
-public interface IPluginKeywordM {
-  public int Id { get; }
-}
--- plugins/Plugins/src/PictureManager.Plugins.Common/Interfaces/Repositories/IPluginPersonR.cs
+++
@@ -1,9 +0,0 @@
-﻿using MH.Utils.Interfaces;
-using PictureManager.Plugins.Common.Interfaces.Models;
-using System.Collections.Generic;
-
-namespace PictureManager.Plugins.Common.Interfaces.Repositories;
-
-public interface IPluginPersonR {
-  public List<IPluginPersonM> Link(string csv, IDataAdapter seeker);
-}
